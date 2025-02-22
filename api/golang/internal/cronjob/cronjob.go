0c8c54564e258ba5fedc6447b767c5ee05bfda69
Marcel Jacek <marcel.jacek@gmail.com>
2025-02-22 16:03:31 +0100

    fix: daily sum calculation

--- api/golang/internal/cronjob/cronjob.go
+++ api/golang/internal/cronjob/cronjob.go
@@ -47,16 +47,16 @@ func initCronJobs(c *cron.Cron, pvRepo internal.PvRepository, db internal.Databa
 		updateForecast(ctx, db, forecastRepo)
 	})
 
-	// Every 6 hours clean history. Once a day would be enough, but in case the service an error occured or the service was done
+	// Every 6 hours cleans history which is older than 24 hours
 	c.AddFunc("0 0 */6 * * *", func() {
 		ctx := context.Background()
 		durationOneDay := time.Hour * 24
 		t := time.Now().Add(durationOneDay * (-1))
 		err := db.CleanHistoryUntil(ctx, t)
 		if err != nil {
-			fmt.Printf("[cleanup] - failed cleaning history %v\n", err)
+			fmt.Printf("[cleanup] - failed cleaning history until %q %v\n", t.String(), err)
 		} else {
-			fmt.Printf("[cleanup] - cleaned history\n")
+			fmt.Printf("[cleanup] - cleaned history until %q\n", t.String())
 		}
 	})
 }
